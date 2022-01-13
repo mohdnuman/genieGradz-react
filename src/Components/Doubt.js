3f624d35f0bbca7f9036b08470bcf84e81ae3036
mohdnuman <mohammadnuman123@gmail.com>
2022-01-13 17:46:39 +0530

    added style to the home page

--- src/Components/Doubt.js
+++ src/Components/Doubt.js
@@ -5,13 +5,14 @@ class Doubt extends Component {
         return (
             <div className='doubt-wrapper'>
                 <div className='doubt-user'>
-                    Numan
+                    <img src="https://cdn-icons.flaticon.com/png/512/2202/premium/2202112.png?token=exp=1642075943~hmac=236879b571aa50fa21c8c1ce55a9f7a2"  className='user-picture'/>
+                    <span className='user-name'>Numan</span>
                 </div>
                 <div className='doubt-topic'> 
-                    Recurion
+                    Topic-Recursion
                 </div>
                 <div className='doubt-description'>
-                    second Doubt
+                    Description-second Doubt
                 </div>
                 <button className='doubt-button'>
                     ACCEPT

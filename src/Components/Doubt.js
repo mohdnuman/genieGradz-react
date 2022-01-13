072f65384e88cfe7b73676d20c565d49bd15123a
mohdnuman <mohammadnuman123@gmail.com>
2022-01-13 16:33:48 +0530

    made the navbar and setup redux

---
+++ src/Components/Doubt.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Doubt extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Doubt;
